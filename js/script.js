6e73ab3e71a5f2117f4c1cb2f5eef35ff017ec5a
timmit147 <timmeeuwsen@hotmail.nl>
2022-04-22 12:12:20 +0200

    keys changed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 var currentKey = 1;
 var currentRow = 1;
+var sound = false;
+var musicOn = true;
 
 function music(){
-    var audio = new Audio(`music/${currentKey}.wav`);
-    audio.play();
-    window.setInterval(function(){
-        audio.pause();
-    }, 700);
+    if(sound == true){
+        var audio = new Audio(`music/${currentKey}.wav`);
+        audio.play();
+        window.setInterval(function(){
+            audio.pause();
+        }, 700);
+    }
 }
 
 
@@ -14,8 +18,8 @@ document.addEventListener("keyup", function(event) {
 
 
     if(move == false){
-        // When clicking arrow down do this function
-        if (event.keyCode === 40) {
+        // When clicking arrow down/j do this function
+        if (event.keyCode === 74) {
             if(currentKey != document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount){
                 currentKey = currentKey+1;
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey-1})`).classList.remove("active");
@@ -28,8 +32,8 @@ document.addEventListener("keyup", function(event) {
             music();
         }
 
-        // When clicking arrow up do this function
-        if (event.keyCode === 38) {
+        // When clicking arrow up/k do this function
+        if (event.keyCode === 75) {
             if(currentKey == 1 ){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentKey = document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount;
@@ -42,8 +46,8 @@ document.addEventListener("keyup", function(event) {
             music();
         }
 
-        // When clicking arrow right do this function
-        if (event.keyCode === 39) {
+        // When clicking arrow right/l do this function
+        if (event.keyCode === 76) {
             if(currentRow == document.querySelector(`main`).childElementCount){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentRow = 1;
@@ -59,8 +63,8 @@ document.addEventListener("keyup", function(event) {
         }
         
 
-        // When clicking arrow left do this function
-        if (event.keyCode === 37) {
+        // When clicking arrow left/h do this function
+        if (event.keyCode === 72) {
             if(currentRow == 1){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentRow = document.querySelector(`main`).childElementCount;
@@ -77,18 +81,21 @@ document.addEventListener("keyup", function(event) {
 
         // When key n clicked do this function
         if (event.keyCode === 78) {
-            const currentUl = document.querySelector(`main :nth-child(${currentRow}) ul`);
+            const currentLi = document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`);
             const li = document.createElement("li");
             let item = prompt("Please enter item", "");
             if(item != ""){
                 li.appendChild(document.createTextNode(item));
-                currentUl.append(li);
+                currentLi.after(li);
+                document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
+                document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey+1})`).classList.add("active");
+                currentKey = currentKey+1;
             }
         }
 
-        // When key B clicked do this function
-        if (event.keyCode === 66) {
-            if(document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount != 1){
+        // When key U clicked do this function
+        if (event.keyCode === 85) {
+            // if(document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount != 1){
                 if(currentKey != document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount){
                     document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).remove();
                     document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.add("active");
@@ -100,125 +107,147 @@ document.addEventListener("keyup", function(event) {
                         currentKey = currentKey-1;
                     }
                 }
-            }
-            else{
-                document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).innerHTML = "No items";
-            }
+                 if(document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount == 0){
+                    currentRow = currentRow+1;
+                    document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.add("active");
+                 }
+
+            // }
+            // else{
+            //     document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).innerHTML = "No items";
+            // }
         }
 
+         // When key P clicked do this function
+         if (event.keyCode === 80) {
+             if(musicOn == true){
+                 console.log(musicOn);
+                if(document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount > 4){
+                    function delay(time) {
+                        return new Promise(resolve => setTimeout(resolve, time));
+                    }
         
-         // When key q clicked do this function
-         if (event.keyCode === 81) {
-             if(document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount > 4){
-                function delay(time) {
-                    return new Promise(resolve => setTimeout(resolve, time));
-                  }
-    
-                  function song(key){
-                    document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
-                    currentKey = key;
-                    document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.add("active");
-                    music();
-                  }
-    
-                  async function test() {
-                    song(5);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(700);
-                    
-                    song(3);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(1000);
-    
-                    song(4);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(1000);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(1000);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(700);
-    
-                    song(3);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(1000);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(700);
-    
-                    song(5);
-                    await delay(700);
-    
-                    song(4);
-                    await delay(700);
-    
-                    song(3);
-                    await delay(700);
-    
-                  }
-              
-                test();
+                    function song(key){
+                        document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
+                        currentKey = key;
+                        document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.add("active");
+                        music();
+                    }
+        
+                    async function test() {
+                        musicOn = false;
+                        song(5);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(700);
+                        
+                        song(3);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(1000);
+        
+                        song(4);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(1000);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(1000);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(700);
+        
+                        song(3);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(1000);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(700);
+        
+                        song(5);
+                        await delay(700);
+        
+                        song(4);
+                        await delay(700);
+        
+                        song(3);
+                        await delay(700);
 
-             }
+                        musicOn = true;
+        
+                    }
+                
+                    test();
+
+                }
+            }
             
         }
 
 
-        // When key V clicked do this function
-        if (event.keyCode === 86) {
+        // When key I clicked do this function
+        if (event.keyCode === 73) {
             let item = prompt("Please enter item", document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).innerHTML);
             if(item != ""){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).innerHTML = item;
             }
         }
+        // When key O clicked turn of music
+        if (event.keyCode === 79) {
+            if (sound == false){
+                sound = true;
+            }
+            else{
+                sound = false;
+            }
+            document.querySelector("#sound").classList.toggle("sound");
 
-        // When key 1 clicked do this function
-        if (event.keyCode === 49) {
+            
+        }
+
+        // When key 4 clicked do this function
+        if (event.keyCode === 52) {
             if(document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(1)`)){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentKey = 1;
@@ -227,8 +256,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When key 2 clicked do this function
-        if (event.keyCode === 50) {
+        // When key 5 clicked do this function
+        if (event.keyCode === 53) {
             if(document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(2)`)){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentKey = 2;
@@ -237,8 +266,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When key 3 clicked do this function
-        if (event.keyCode === 51) {
+        // When key 6 clicked do this function
+        if (event.keyCode === 54) {
             if(document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(3)`)){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentKey = 3;
@@ -247,8 +276,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When key 4 clicked do this function
-        if (event.keyCode === 52){
+        // When key 7 clicked do this function
+        if (event.keyCode === 55){
             if(document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(4)`)){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentKey = 4;
@@ -257,8 +286,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When key 5 clicked do this function
-        if (event.keyCode === 53){
+        // When key 8 clicked do this function
+        if (event.keyCode === 56){
             if(document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(5)`)){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentKey = 5;
@@ -267,8 +296,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When key 6 clicked do this function
-        if (event.keyCode === 54){
+        // When key 9 clicked do this function
+        if (event.keyCode === 57){
             if(document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(6)`)){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentKey = 6;
@@ -277,8 +306,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When key 7 clicked do this function
-        if (event.keyCode === 55){
+        // When key 0 clicked do this function
+        if (event.keyCode === 48){
             if(document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(7)`)){
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
                 currentKey = 7;
@@ -292,8 +321,8 @@ document.addEventListener("keyup", function(event) {
 
     // if m is not enterd
     else{
-        // When clicking arrow down do this function
-        if (event.keyCode === 40) {
+        // When clicking arrow down/j do this function
+        if (event.keyCode === 74) {
             if(currentKey < document.querySelector(`main :nth-child(${currentRow}) ul`).childElementCount){
                 const text = document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).innerHTML;
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).innerHTML = document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey+1})`).innerHTML;
@@ -304,8 +333,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When clicking arrow up do this function
-        if (event.keyCode === 38) {
+        // When clicking arrow up/k do this function
+        if (event.keyCode === 75) {
             if(currentKey > 1){
                 const text = document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).innerHTML;
                 document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).innerHTML = document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey-1})`).innerHTML;
@@ -316,8 +345,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When clicking arrow right do this function
-        if (event.keyCode === 39) {
+        // When clicking arrow right/l do this function
+        if (event.keyCode === 76) {
             if(currentRow < document.querySelector(`main`).childElementCount){
                 const currentLi = document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`);
                 currentKey = 1;
@@ -338,8 +367,8 @@ document.addEventListener("keyup", function(event) {
             }
         }
 
-        // When clicking arrow left do this function
-        if (event.keyCode === 37) {
+        // When clicking arrow left/h do this function
+        if (event.keyCode === 72) {
             if(currentRow > 1){
                 const currentLi = document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`);
                 currentKey = 1;
@@ -360,7 +389,7 @@ document.addEventListener("keyup", function(event) {
         }
     }
 
-    // When clicking key m do this function
+    // When clicking key M do this function
     if (event.keyCode === 77) {
         if(move == false){
             document.querySelector(`main :nth-child(${currentRow}) ul :nth-child(${currentKey})`).classList.remove("active");
